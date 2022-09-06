0a000543ecc2b055744e8fe4f48de4e00fe5ff77
Mariama Diallo <diallomt193@yahoo.com>
2022-09-06 11:23:02 -0400

    updated the diabetes schema

--- Diabetes.sql
+++ Diabetes.sql
@@ -1,1 +1,21 @@
-SELECT * FROM diabetes
+CREATE TABLE "diabetes" (
+    "id" INT PRIMARY KEY,
+    "Pregnancies" VARCHAR   NOT NULL,
+    "Glucose" VARCHAR   NOT NULL,
+    "BloodPressure" VARCHAR   NOT NULL,
+    "SkinThickness" VARCHAR   NOT NULL,
+    "Insulin" VARCHAR   NOT NULL,
+    "BMI_x" VARCHAR   NOT NULL,
+    "DiabetesPedigreeFunction" VARCHAR   NOT NULL,
+    "Age" VARCHAR   NOT NULL,
+    "Outcome" VARCHAR   NOT NULL,
+    "Diabetes_012" VARCHAR   NOT NULL,
+    "HighBP" VARCHAR   NOT NULL,
+    "HighChol" VARCHAR   NOT NULL,
+	"CholCheck" VARCHAR   NOT NULL,
+    "BMI_y" VARCHAR   NOT NULL,
+    "Smoker" VARCHAR   NOT NULL,
+    "Stroke" VARCHAR   NOT NULL
+);
+
+select * from diabetes
